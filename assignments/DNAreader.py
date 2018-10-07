ee7da0fdb6601f9f6d1c8c302871f9f88f96ea2f
Willie Hurst <whurst1@lsu.edu>
2018-10-07 07:32:24 -0700

    DNA READER Whurst1

---
+++ assignments/DNAreader.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+DNASeq = input("Enter DNA Sequence to be Analized: ")
+#convert to upper
+DNASeq = DNASeq.upper()
+
+print ("DNA Sqecuence to be Analized: ")
+print (DNASeq)
+print (" ")
+print ("Sequence Length: ", end =" ")
+print (len(DNASeq))
+print (" ")
+#Number of A's:
+print ("Count of A's: ", end = " ")
+print (DNASeq.count("A"))
+#Number of C's:
+print ("Count of C's: ", end = " ")
+print (DNASeq.count("C"))
+#Number of G's:
+print ("Count of G's: ", end = " ")
+print (DNASeq.count ("G"))
+#Number of T's:
+print ("Count of T's: ", end = " ")
+print (DNASeq.count ("T"))
