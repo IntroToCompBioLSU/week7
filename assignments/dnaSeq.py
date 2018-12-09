b6538812a85253fad8e374130eabcebb5c954983
jembrown <jeremymbrown@gmail.com>
2018-12-09 00:45:46 -0600

    Comments added.

--- assignments/dnaSeq.py
+++ assignments/dnaSeq.py
@@ -8,20 +8,19 @@ print(userSeq.count("a") + userSeq.count("g") + userSeq.count("c") + userSeq.cou
 
 # count number of As0
 print("Number of As")
-print(userSeq.count("a"))
+print(userSeq.lower().count("a"))
 
 # Count number of Cs
 print("Number of Cs")
-print(userSeq.count("c"))
+print(userSeq.lower().count("c"))
 
 # Count number of Gs
 print("Number of Gs")
-print(userSeq.count("g"))
+print(userSeq.lower().count("g"))
 
 # Count number of Ts
 print("Number of Ts")
-print(userSeq.count("t"))
+print(userSeq.lower().count("t"))
 
-
-
-
+# DB: Good! My only suggestion is that you can make it more general by allowing both
+#     upper- and lowercase sequences if you add .lower() to userSeq (see above).
